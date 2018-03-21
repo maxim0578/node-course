0148d62c513a0daab88971cf34d73ae6fd9b9be6
maxim <maxim.nihongo@gmail.com>
2018-03-21 15:08:03 +0900

    set up start and heraku port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-
+const port = process.env.port || 3000;
 
 
 var app = express(); //make new express app. calling express as function
@@ -95,4 +95,6 @@ app.get('/error',(req,res)=>{
   })
 });
 
-app.listen(3000);
+app.listen(port, ()=>{
+  console.log(`server is up on${port}`);
+});
